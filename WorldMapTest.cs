c6895e4003070697445dfdf7b9ad39f8e5ae09e6
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2022-11-07 19:55:53 +0100

    modified WorldMapTest

--- WorldMapTest.cs
+++ WorldMapTest.cs
@@ -16,7 +16,7 @@ namespace worldMapTest
 	[TestClass]
 	public class WorldMapTest
 	{
-		
+		/*
 		WorldMap worldMap = new WorldMapImpl();
 		
 
@@ -32,9 +32,7 @@ namespace worldMapTest
 		private void InitializeInstanceFields()
 		{
 			worldMap = new WorldMapImpl(WIDTH, HEIGHT, NUM_ENEMIES, NUM_COLLECTABLES, randomStrategy);
-		}
-
-
+		}*/
 		private static int WIDTH = 51;
 		private static int HEIGHT = 51;
 		private static int NUM_ENEMIES = 5;
@@ -47,7 +45,7 @@ namespace worldMapTest
 		private Point2D randomPos = new Point2D(3, EXPECTED_SIZE);
 
 
-
+		[TestMethod]
 		public virtual void testRandomSpawnStrategy()
 		{
 			int width = 10;
@@ -58,19 +56,19 @@ namespace worldMapTest
 			ISet<Point2D> set2 = this.randomStrategy.getSpawnPoints(width, height, numEntities);
 			set2.Add(randomPos);
 			ISet<Point2D> allSet = this.randomStrategy.getDoubleSpawnPoints(width, height, set1, set2);
-			assertEquals(EXPECTED_SIZE, set1.Count);
-			assertEquals(EXPECTED_SIZE * 2, allSet.Count);
-			assertTrue(allSet.ContainsAll(set1));
-			assertTrue(allSet.ContainsAll(set2));
+			Assert.AreEqual(EXPECTED_SIZE, set1.Count);
+			Assert.True(EXPECTED_SIZE * 2, allSet.Count);
+			Assert.True(allSet.ContainsAll(set1));
+			Assert.True(allSet.ContainsAll(set2));
 		}
 
 		public virtual void testWorldMapCreation()
 		{
 			IDictionary<Point2D, Optional<Entity>> board = worldMap.getBoard();
 			long count = board.Values.Where(v => v.isPresent()).Count();
-			assertEquals(WIDTH * HEIGHT, board.Count);
-			assertTrue(board[startPlayerPos].get() is Player);
-			assertEquals(NUM_ENEMIES + NUM_COLLECTABLES + 1, count);
+			Assert.AreEqual(WIDTH * HEIGHT, board.Count);
+			Assert.True(board[startPlayerPos].get() is Player);
+			Assert.AreEqual(NUM_ENEMIES + NUM_COLLECTABLES + 1, count);
 		}
 
 
@@ -80,9 +78,9 @@ namespace worldMapTest
 			worldMap.movePlayer(MOVEMENT.UP);
 			IDictionary<Point2D, Optional<Entity>> board = worldMap.getBoard();
 			Point2D leftPosition = new Point2D(24, 26);
-			assertTrue(board[leftPosition].isPresent());
-			assertTrue(board[leftPosition].get() is Player);
-			assertTrue(board[startPlayerPos].isEmpty());
+			Assert.True(board[leftPosition].isPresent());
+			Assert.True(board[leftPosition].get() is Player);
+			Assert.True(board[startPlayerPos].isEmpty());
 		}
 	}
 
