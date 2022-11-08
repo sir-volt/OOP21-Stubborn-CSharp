749a3f3779fbbde05b2eed11490e56dafe8ee123
Mario Ciccioni <mario.ciccioni@studio.unibo.it>
2022-11-08 21:46:52 +0100

    interface IAiEnemy and class RandomAiEnemy added

---
+++ models/IAiEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace models
+{
+
+	public interface IAiEnemy
+	{
+
+		Point2D Move(IDictionary<Point2D, Optional<Entity>> board, Point2D playerPosition, Point2D position);
+
+	}
+
+}
